d96eed17863be3419190a01db23c84123e04b475
Vedant Giri <113013212+vedant-giri@users.noreply.github.com>
2026-04-18 13:36:35 +0530

    Updated API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://task-manager-backend-2chm.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
